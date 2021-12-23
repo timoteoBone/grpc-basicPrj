36c9e9109dcf48f03afc6d2632ae40d190e9b898
timoteoBone <timoteo.bone@globant.com>
2021-12-23 19:10:37 -0300

    hay errores con el sv en main

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"github.com/hashicorp/go-hclog"
+	protos "github.com/timoteoBone/grpc-basicPrj/fitness"
+	"github.com/timoteoBone/grpc-basicPrj/server"
+	"google.golang.org/grpc"
+)
+
 func main() {
+
+	log := hclog.Default()
+	sv := server.NewServer(log)
+
+	grpcSv := &grpc.Server{}
+
+	protos.RegisterNutricionistServer(grpcSv, sv)
 
 }
--- server/server.go
+++ server/server.go
@@ -3,11 +3,19 @@ package server
 import (
 	"context"
 
+	"github.com/hashicorp/go-hclog"
 	protos "github.com/timoteoBone/grpc-basicPrj/fitness"
 )
 
-type Server struct{}
+type Server struct {
+	log hclog.Logger
+}
 
-func (s *Server) SendRoutine(ctx *context.Context, ud *protos.UserData) (*protos.DietResponse, error) {
+func NewServer(log hclog.Logger) *Server {
+	return &Server{log}
+}
+
+func (s *Server) SendRoutine(con context.Context, ud *protos.UserData) (*protos.DietResponse, error) {
+	s.log.Info("sd")
 	return &protos.DietResponse{Basis: "vegetables", Calories: 3500}, nil
 }
